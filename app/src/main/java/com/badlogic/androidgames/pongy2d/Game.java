3b8cd8f7e0a00e25e8795af48644464845fd9de7
hakan çakmak <hakan.cakmak@live.com>
2021-03-16 14:18:17 +0300

    manifest düzenlendi. Activity class yapılandırıldı. Game sınıfı temeli oluşturuldu. constructor ve metotlar hazırlanacak

--- app/src/main/java/com/badlogic/androidgames/pongy2d/Game.java
+++ app/src/main/java/com/badlogic/androidgames/pongy2d/Game.java
@@ -1,4 +1,38 @@
 package com.badlogic.androidgames.pongy2d;
 
-public class Game {
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+// 5 - Let's extend surfaceview and add the necessary variables
+public class Game extends SurfaceView {
+
+    private SurfaceHolder mHolder;
+    private Canvas mCanvas;
+    private Paint mPaint;
+
+    private long mFPS;
+    private final int MILLIS_IN_SECOND = 1000;
+
+    private int mScreenX;
+    private int mScreenY;
+    private int mFontSize;
+    private int mFontMargin;
+
+    // 6- Let's add objects of other classes
+    private Paddle mPaddle;
+    private Ball mBall;
+
+    private int score = 0;
+    private int lives = 3;
+
+    // 7-Let's set up thread structure
+    private Thread mGameThread = null;
+    private volatile boolean mPlaying;
+    private boolean mPaused = true;
+
+    // 8- constructor will be installed
+    // 9- startnewgame will be written
+
 }
--- app/src/main/java/com/badlogic/androidgames/pongy2d/PongyActivity.java
+++ app/src/main/java/com/badlogic/androidgames/pongy2d/PongyActivity.java
@@ -1,15 +1,44 @@
 package com.badlogic.androidgames.pongy2d;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
 // 1- Let's extend the activity class and call oncreate. Then let's create our game, paddel and ball classes.
 public class PongyActivity extends Activity {
 
+    private Game mGame;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 2- Let's do the basic setups
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mGame = new Game(this, size.x, size.y);
+        setContentView(mGame);
+    }
+
+   // 3- Let's override resume and pauses methods
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mGame.resume();
+    }
+
+    // 4- and let's move to the Game class
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mGame.pause();
     }
 }
