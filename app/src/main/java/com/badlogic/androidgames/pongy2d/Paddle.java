32e9f9da25bd59cc29582ff89b0bf4e37b7152bc
hakan çakmak <hakan.cakmak@live.com>
2021-03-16 13:19:02 +0300

    Oyun Activity den extend edildi. diğer gerekli sınıflar oluşturuldu. oncreate çağrıldı.

---
+++ app/src/main/java/com/badlogic/androidgames/pongy2d/Paddle.java
@@ -0,0 +1,4 @@
+package com.badlogic.androidgames.pongy2d;
+
+public class Paddle {
+}
---
+++ app/src/main/java/com/badlogic/androidgames/pongy2d/PongyActivity.java
@@ -0,0 +1,15 @@
+package com.badlogic.androidgames.pongy2d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+// 1- Let's extend the activity class and call oncreate. Then let's create our game, paddel and ball classes.
+public class PongyActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
